a46b9b78467b717ce87e908989371d74fef9e9e1
wuyeelok <wuyeelok@gmail.com>
2022-01-11 10:34:19 +0800

    Expose root to show server using REST

---
+++ src/main/java/com/example/demo/rest/FunRestController.java
@@ -0,0 +1,17 @@
+package com.example.demo.rest;
+
+import java.time.LocalDateTime;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+	 // expose "/"
+	
+	@GetMapping("/")
+	public String sayHello() {
+		return "Hello World, time on server is " + LocalDateTime.now();
+	}
+}
