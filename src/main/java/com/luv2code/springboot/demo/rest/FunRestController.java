337e26e176d515597d38a31e0fcc269c7b600e7d
wuyeelok <wuyeelok@gmail.com>
2022-01-17 16:14:47 +0800

    Expose 2 url for testing Spring Boot Dev Tools

--- src/main/java/com/luv2code/springboot/demo/rest/FunRestController.java
+++ src/main/java/com/luv2code/springboot/demo/rest/FunRestController.java
@@ -8,10 +8,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FunRestController {
 
-	 // expose "/"
-	
+	// expose "/"
 	@GetMapping("/")
 	public String sayHello() {
 		return "Hello World, time on server is " + LocalDateTime.now();
+	}
+
+	// Expose a new endpoint for "workout"
+	@GetMapping("/workout")
+	public String getDailyWorkout() {
+		return "Run a hard 5k";
+	}
+
+	// Expose a new endpoint for "fortune"
+	@GetMapping("/fortune")
+	public String getDailyFortune() {
+		return "Today is your lucky day.";
 	}
 }
