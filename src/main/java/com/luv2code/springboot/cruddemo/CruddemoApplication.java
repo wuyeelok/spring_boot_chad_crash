2bf8440f73ed2310ea3018a0e4728d46ad026849
wuyeelok <wuyeelok@gmail.com>
2022-02-03 21:55:43 +0800

    Setup Cruddemo Spring boot project

--- src/main/java/com/luv2code/springboot/cruddemo/CruddemoApplication.java
+++ src/main/java/com/luv2code/springboot/cruddemo/CruddemoApplication.java
@@ -1,13 +1,13 @@
-package com.luv2code.springboot.demo;
+package com.luv2code.springboot.cruddemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootChadCrashApplication {
+public class CruddemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootChadCrashApplication.class, args);
+		SpringApplication.run(CruddemoApplication.class, args);
 	}
 
 }
--- src/test/java/com/luv2code/springboot/cruddemo/CruddemoApplicationTests.java
+++ src/test/java/com/luv2code/springboot/cruddemo/CruddemoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.luv2code.springboot.demo;
+package com.luv2code.springboot.cruddemo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringBootChadCrashApplicationTests {
+class CruddemoApplicationTests {
 
 	@Test
 	void contextLoads() {
